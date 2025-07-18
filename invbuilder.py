65445760eebcd03e52cde899cddd24501673be12
DominusUrsus <mccoy_c97@pm.me>
2025-07-18 07:52:23 -0400

    Moved things back

--- invbuilder.py
+++ invbuilder.py
@@ -1,7 +1,4 @@
 
-item_dict = {}
-inv_list = []
-
 def get_blueprint_status(item):
     pass
 
@@ -14,13 +11,3 @@ def get_item_id(item):
 def get_item_value(item):
     pass
 
-#Open and parse csv file given as argument + create a global inventory
-def csv_parser(file_path):
-    with open(file_path, newline = '') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)
-        for row in reader:
-            item_dict ={"item": row[0], "count": int(row[1])}
-            inv_list.append(item_dict)
-    print(f"Debug: Inventory list:{inv_list}")
-    return inv_list
